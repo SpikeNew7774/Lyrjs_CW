7726b9a394ad356bbfa00c4493b6cf642e1199b0
SpikeNew7774 <newadm159@gmail.com>
2024-10-03 20:18:21 +0200

    Use Line, unless ?forcemx=true

--- src/index.js
+++ src/index.js
@@ -210,6 +210,7 @@ const fetchMusixmatchLyrics = async (trackData, c, blData) => {
 // Route: /lyrics/id (with multiple IDs support)
 app.get('/lyrics/id', async (c) => {
   oenv = c.env;
+  const forceMxMatch = c.req.query("forcemx") === "true";
   const trackId = c.req.query('id');
   const ids = c.req.query('ids')?.split(',');
   let userAccessToken = c.req.header('Authorization');
@@ -263,7 +264,7 @@ app.get('/lyrics/id', async (c) => {
       const lyrics = JSON.parse(lyricsResponse);
       const type = lyrics.Type || null;
 
-      if (type === "Syllable" || type === "Line") {
+      if (forceMxMatch || type === "Syllable" || type === "Line") {
         // If Beautiful-Lyrics has "Syllable", just use it
         fullLyricsList.content.push({
           name: data.name,
